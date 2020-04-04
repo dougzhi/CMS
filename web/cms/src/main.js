256cc638835d4c447fc8cee8e170f17b1ba9bd8e
dongz <89518029@qq.com>
2020-04-05 00:24:33 +0800

    move web

--- web/cms/src/main.js
+++ web/cms/src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import VueRouter from 'vue-router'
-import routes from '../src/base/router'
-import store from '../src/vuex/store'
+import routes from './base/router'
+import store from './vuex/store'
 
 // import { Message } from 'element-ui';
 // import utilApi from '../src/common/utils';
